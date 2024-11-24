fe1bd44ef91ca75a88439ce7a451751aeb69f83e
Juan <lopezsurianjuancarlos5@gmail.com>
2024-11-23 20:35:38 -0600

    otro intento

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 
-<link rel="stylesheet" href="{{asset('css/layout.css')}}">
 
 </head>
 
